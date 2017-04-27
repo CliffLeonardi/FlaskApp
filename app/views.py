3969bb0e86e82ccb8deea4eee7ce5158bb299da2
Cliff Leonardi <clifford@maxwellhealth.com>
2017-04-27 18:43:09 -0400

    Web forms (#2)
    * web-forms:
    add base command for flaskWTF
    Begin implementing openid just so we can kill it with fire later
    
    * Add some new routes for login
    
    * New shit!
    
    * web-forms:
    finish html openid login

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
-from flask import render_template
+# Imports
+from flask import render_template, flash, redirect
 from app import app
+#Import Class
+from .forms import LoginForm
 
-@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data))
+        )
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form,
+        providers=app.config['OPENID_PROVIDERS'])
+
+# We don't need to get rid of this yet. If we toss it then the index page goes down.
 @app.route('/index')
 
 def index():

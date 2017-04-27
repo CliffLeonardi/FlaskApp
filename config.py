42175acec622d377026bd6a39f6df9767daf7430
cliffleonardi <clifford@maxwellhealth.com>
2017-04-27 19:43:51 -0400

    Initial setup for DB and models

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(_file_))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "Secret_Key"
 

6c442ac9bd6c37ebe82b9c30ba03b9f72c0f30be
cliffleonardi <clifford@maxwellhealth.com>
2017-05-11 19:58:55 -0400

    add upgrade and downgrade script

---
+++ db_upgrade.py
@@ -0,0 +1,9 @@
+#!flask/bin/python3.5
+
+from migrate.versioning import api
+from conig import SQLALCHEMY_DATABASE_URI
+from conig import SQLALCHEMY_DATABASE_REPO
+
+api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+print('Current database version: ' + str(v))
